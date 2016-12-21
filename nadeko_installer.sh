bb64905a63e17cb7084e846e4a16c040750ba53c
dr3nn <dr3ntheking@gmail.com>
2016-12-21 09:23:24 -0600

    Update nadeko_installer.sh

--- nadeko_installer.sh
+++ nadeko_installer.sh
@@ -32,7 +32,7 @@ cd "$tempdir"
 
 echo ""
 echo "Downloading NadekoBot, please wait."
-git clone -b master --recursive --depth 1 https://github.com/Kwoth/NadekoBot.git
+git clone -b master --recursive --depth 1 https://github.com/dr3nn/NadekoBot.git
 echo ""
 echo "NadekoBot downloaded."
 
